a2137940b5442ba462a5da27e0dc3afeea56779e
antopalidi <t.opalidisanna@gmail.com>
2022-05-27 18:45:05 +0200

    refactoring  tests - model game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -23,89 +23,103 @@ RSpec.describe Game, type: :model do
 
     let(:game) { Game.create_game_for_user!(user) }
 
-    it 'should create a game with specified user' do
-      expect(game.user).to eq(user)
-    end
+    context 'game creation process' do
+      it 'should create a game with specified user' do
+        expect(game.user).to eq(user)
+      end
 
-    it 'should create a game with status in progress' do
-      expect(game.status).to eq(:in_progress)
-    end
+      it 'should create a game with status in progress' do
+        expect(game.status).to eq(:in_progress)
+      end
 
-    it 'should create a game with 15 questions' do
-      expect(game.game_questions.size).to eq(15)
-    end
+      it 'should create a game with 15 questions' do
+        expect(game.game_questions.size).to eq(15)
+      end
 
-    it 'should create a game with questions levels from 0 to 14' do
-      expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+      it 'should create a game with questions levels from 0 to 14' do
+        expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+      end
     end
   end
 
   # Тесты на основную игровую логику
-  context 'game mechanics' do
+  describe 'game mechanics' do
     # Правильный ответ должен продолжать игру
-    it 'answer correct continues game' do
-      level = game_w_questions.current_level
-      q = game_w_questions.current_game_question
+    context 'when answer is correct' do
+      it 'should continue the game' do
+        level = game_w_questions.current_level
+        q = game_w_questions.current_game_question
 
-      expect(game_w_questions.status).to eq(:in_progress)
+        expect(game_w_questions.status).to eq(:in_progress)
 
-      game_w_questions.answer_current_question!(q.correct_answer_key)
+        game_w_questions.answer_current_question!(q.correct_answer_key)
 
-      # Перешли на след. уровень
-      expect(game_w_questions.current_level).to eq(level + 1)
+        # Перешли на след. уровень
+        expect(game_w_questions.current_level).to eq(level + 1)
 
-      # Ранее текущий вопрос стал предыдущим
-      expect(game_w_questions.current_game_question).not_to eq(q)
+        # Ранее текущий вопрос стал предыдущим
+        expect(game_w_questions.current_game_question).not_to eq(q)
 
-      # Игра продолжается
-      expect(game_w_questions.status).to eq(:in_progress)
-      expect(game_w_questions.finished?).to be_falsey
+        # Игра продолжается
+        expect(game_w_questions.status).to eq(:in_progress)
+        expect(game_w_questions.finished?).to be_falsey
+      end
     end
 
-    it 'take_money! finishes the game' do
-      # берем игру и отвечаем на текущий вопрос
-      q = game_w_questions.current_game_question
-      game_w_questions.answer_current_question!(q.correct_answer_key)
+    context 'when take the money' do
+      it 'should finish the game' do
+        # берем игру и отвечаем на текущий вопрос
+        q = game_w_questions.current_game_question
+        game_w_questions.answer_current_question!(q.correct_answer_key)
 
-      # взяли деньги
-      game_w_questions.take_money!
+        # взяли деньги
+        game_w_questions.take_money!
 
-      prize = game_w_questions.prize
-      expect(prize).to be > 0
+        prize = game_w_questions.prize
+        expect(prize).to be > 0
 
-      # проверяем что закончилась игра и пришли деньги игроку
-      expect(game_w_questions.status).to eq :money
-      expect(game_w_questions.finished?).to be_truthy
-      expect(user.balance).to eq prize
+        # проверяем что закончилась игра и пришли деньги игроку
+        expect(game_w_questions.status).to eq :money
+        expect(game_w_questions.finished?).to be_truthy
+        expect(user.balance).to eq prize
+      end
     end
   end
 
   # группа тестов на проверку статуса игры
-  context 'status' do
+  describe 'status' do
     # перед каждым тестом "завершаем игру"
     before(:each) do
       game_w_questions.finished_at = Time.now
       expect(game_w_questions.finished?).to be_truthy
     end
 
-    it ':won' do
-      game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
-      expect(game_w_questions.status).to eq(:won)
+    context 'when the game is won' do
+      it 'should return :won' do
+        game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
+        expect(game_w_questions.status).to eq(:won)
+      end
     end
 
-    it ':fail' do
-      game_w_questions.is_failed = true
-      expect(game_w_questions.status).to eq(:fail)
+    context 'when the game is fail' do
+      it 'should return :fail' do
+        game_w_questions.is_failed = true
+        expect(game_w_questions.status).to eq(:fail)
+      end
     end
 
-    it ':timeout' do
-      game_w_questions.created_at = 1.hour.ago
-      game_w_questions.is_failed = true
-      expect(game_w_questions.status).to eq(:timeout)
+    context 'when time expired' do
+      it 'should return :timeout' do
+        game_w_questions.created_at = 1.hour.ago
+        game_w_questions.is_failed = true
+        expect(game_w_questions.status).to eq(:timeout)
+      end
     end
 
-    it ':money' do
-      expect(game_w_questions.status).to eq(:money)
+    context 'when decided to take the money' do
+      it 'should return :money' do
+        expect(game_w_questions.status).to eq(:money)
+      end
     end
   end
 
